fd02fa33cef75f70e7f073aaa3ab5d9f1943aebf
Toby Coleman <toby@tobycoleman.com>
2024-09-07 11:07:27 +0100

    Add enum, ConnectorBuilderSpec

--- plugboard/schemas/connector.py
+++ plugboard/schemas/connector.py
@@ -1,6 +1,17 @@
-"""Provides `ConnectorSpec` class."""
+"""Provides spec classes related to `Connector`s."""
+
+from enum import StrEnum
 
 from pydantic import BaseModel
+
+
+class ConnectorMode(StrEnum):
+    """Defines the mode of a connector."""
+
+    ONE_TO_ONE = "one-to-one"
+    ONE_TO_MANY = "one-to-many"
+    MANY_TO_ONE = "many-to-one"
+    MANY_TO_MANY = "many-to-many"
 
 
 class ConnectorSpec(BaseModel):
@@ -8,5 +19,15 @@ class ConnectorSpec(BaseModel):
 
     source: str
     target: str
-    # TODO: change to enum
-    mode: str = "ONE_TO_ONE"
+    mode: ConnectorMode = ConnectorMode.ONE_TO_ONE
+
+    @property
+    def id(self) -> str:  # noqa: D102
+        return f"{self.source.id}..{self.target.id}"
+
+
+class ConnectorBuilderSpec(BaseModel):
+    """Specification of a Plugboard `ConnectorBuilder`."""
+
+    source: str
+    target: str
