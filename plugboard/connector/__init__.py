37fe6a6719cafec764420057b650cf6bf717978f
Chris Knight <chrisk314@gmail.com>
2024-09-07 13:29:34 +0100

    fixup! Adds AsyncioChannel implementation of Channel for testing

--- plugboard/connector/__init__.py
+++ plugboard/connector/__init__.py
@@ -1,5 +1,6 @@
 """Connector submodule providing functionality related to component connectors and data exchange."""
 
+from plugboard.connector.asyncio_channel import AsyncioChannel
 from plugboard.connector.channel import Channel
 from plugboard.connector.connector import Connector, ConnectorMode, ConnectorSpec
 
@@ -9,4 +10,5 @@ __all__ = [
     "ConnectorSpec",
     "ConnectorMode",
     "Channel",
+    "AsyncioChannel",
 ]
