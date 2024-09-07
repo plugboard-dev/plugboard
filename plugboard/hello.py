58b3f382428287359b9ddfcdf10d654bac8df765
Chris Knight <chrisk314@gmail.com>
2024-09-07 13:35:37 +0100

    Removes placeholder helloworld code and test

--- plugboard/hello.py
+++
@@ -1,8 +0,0 @@
-"""Placeholder hello world module for plugboard."""
-
-from plugboard import __version__
-
-
-def hello() -> None:
-    """Prints a hello world message."""
-    print(f"Hello, world! from version {__version__}", flush=True)
