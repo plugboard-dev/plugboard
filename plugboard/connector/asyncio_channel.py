6421f836e5aaa8b56aa6fe0d0b7a1db01dc8f26f
Chris Knight <chrisk314@gmail.com>
2024-09-07 12:49:17 +0100

    Adds AsyncioChannel implementation of Channel for testing

---
+++ plugboard/connector/asyncio_channel.py
@@ -0,0 +1,30 @@
+"""Provides `AsyncioChannel` class."""
+
+from asyncio import Queue
+import typing as _t
+
+from plugboard.connector.channel import CHAN_MAXSIZE, Channel
+
+
+class AsyncioChannel(Channel):
+    """`AsyncioChannel` enables async data exchange between coroutines on the same host."""
+
+    def __init__(self, *args: _t.Any, maxsize: int = CHAN_MAXSIZE, **kwargs: _t.Any):  # noqa: D417
+        """Instantiates `AsyncioChannel`.
+
+        Args:
+            maxsize: Optional; Queue maximum item capacity.
+        """
+        self._maxsize: int = maxsize
+        self._queue: Queue = Queue(maxsize=maxsize)
+        super().__init__(*args, **kwargs)  # type: ignore
+
+    async def send(self, item: _t.Any) -> None:
+        """Sends an item through the `Channel`."""
+        await self._queue.put(item)
+
+    async def recv(self) -> _t.Any:
+        """Returns an item received from the `Channel`."""
+        item = await self._queue.get()
+        self._queue.task_done()
+        return item
