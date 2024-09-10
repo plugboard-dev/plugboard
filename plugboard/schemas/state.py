23ddb2ba3e77e92bd52dc3fca494bd67d6676dd5
Toby Coleman <toby@tobycoleman.com>
2024-09-10 20:51:03 +0100

    Update state

--- plugboard/schemas/state.py
+++ plugboard/schemas/state.py
@@ -1,10 +1,18 @@
 """Provides `StateBackendSpec` class."""
 
+import typing as _t
+
 from pydantic import BaseModel
+
+
+class StateBackendArgsSpec(BaseModel, extra="allow"):
+    """Specification of the `StateBackend` constructor arguments."""
+
+    parameters: _t.Optional[dict] = None
 
 
 class StateBackendSpec(BaseModel):
     """Specification of a Plugboard `StateBackend`."""
 
     type: str
-    parameters: dict
+    args: StateBackendArgsSpec
