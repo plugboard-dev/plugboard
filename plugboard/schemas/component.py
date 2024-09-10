2946bc0764ee5ea3324c25cc29c660b286b62550
Toby Coleman <toby@tobycoleman.com>
2024-09-10 20:44:11 +0100

    Update Component from review

--- plugboard/schemas/component.py
+++ plugboard/schemas/component.py
@@ -1,13 +1,21 @@
 """Provides `ComponentSpec` class."""
 
+import typing as _t
+
 from pydantic import BaseModel
+
+
+class ComponentArgsSpec(BaseModel, extra="allow"):
+    """Specification of the `Component` constructor arguments."""
+
+    name: str
+    initial_values: _t.Optional[dict] = None
+    parameters: _t.Optional[dict] = None
+    constraints: _t.Optional[dict] = None
 
 
 class ComponentSpec(BaseModel):
     """Specification of a Plugboard `Component`."""
 
     name: str
-    type: str
-    initial_values: dict
-    parameters: dict
-    constraints: dict
+    args: ComponentArgsSpec
