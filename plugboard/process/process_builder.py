7df7955c5cbb3745b770a37b86e17ecb619f03b9
Toby Coleman <toby@tobycoleman.com>
2024-09-15 19:33:35 +0100

    Code to load components

--- plugboard/process/process_builder.py
+++ plugboard/process/process_builder.py
@@ -1,5 +1,9 @@
 """Provides `ProcessBuilder` to build `Process` objects."""
 
+from pydoc import locate
+import typing as _t
+
+from plugboard.component.component import Component
 from plugboard.process.process import Process
 from plugboard.schemas.process import ProcessSpec
 
@@ -7,8 +11,25 @@ from plugboard.schemas.process import ProcessSpec
 class ProcessBuilder:
     """Builds `Process` objects."""
 
-    @classmethod
-    def build(cls, spec: ProcessSpec) -> Process:
+    def __init__(self, types: list[_t.Type[Component]]):
+        """Instantiates a `ProcessBuilder`.
+
+        Args:
+            types: A list of `Component` types to use in building `Process` objects.
+        """
+        self._component_types = {ct.__name__: ct for ct in types}
+
+    def _load_type(self, type_name: str) -> _t.Type[Component]:
+        try:
+            return self._component_types[type_name]
+        except KeyError:
+            ct = locate(type_name)
+            if ct is None or not issubclass(ct, Component):
+                raise ValueError(f"Component type {type_name} not found.")
+            self._component_types[type_name] = ct
+            return ct
+
+    def build(self, spec: ProcessSpec) -> Process:
         """Build a `Process` object.
 
         Args:
@@ -18,8 +39,7 @@ class ProcessBuilder:
             A `Process` object.
         """
         return Process(
-            # TODO: load components
-            components=[],
+            components=[self._load_type(c.type)(**c.args) for c in spec.args.components],
             # TODO: build connectors
             connectors=[],
             parameters=spec.args.parameters,
