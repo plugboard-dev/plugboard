5bc336efdce9d9928fc252e1c3e46f5f6765e9ea
Toby Coleman <toby@tobycoleman.com>
2024-09-15 18:57:16 +0100

    Add skeleton code

--- plugboard/process/__init__.py
+++ plugboard/process/__init__.py
@@ -1,8 +1,10 @@
 """Process submodule providing functionality related to processes and their execution."""
 
 from plugboard.process.process import Process
+from plugboard.process.process_builder import ProcessBuilder
 
 
 __all__ = [
     "Process",
+    "ProcessBuilder",
 ]
--- plugboard/process/process_builder.py
+++ plugboard/process/process_builder.py
@@ -1,7 +1,26 @@
 """Provides `ProcessBuilder` to build `Process` objects."""
 
+from plugboard.process.process import Process
+from plugboard.schemas.process import ProcessSpec
+
 
 class ProcessBuilder:
     """Builds `Process` objects."""
 
-    pass
+    @classmethod
+    def build(cls, spec: ProcessSpec) -> Process:
+        """Build a `Process` object.
+
+        Args:
+            spec: A `ProcessSpec` object defining the `Process`.
+
+        Returns:
+            A `Process` object.
+        """
+        return Process(
+            # TODO: load components
+            components=[],
+            # TODO: build connectors
+            connectors=[],
+            parameters=spec.args.parameters,
+        )
