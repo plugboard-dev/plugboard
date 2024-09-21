5721e842fd0b59c73ff5b0b6f83b84ff88f71f43
Toby Coleman <toby@tobycoleman.com>
2024-09-21 14:49:31 +0100

    ClassLoader no longer needed

--- plugboard/utils/__init__.py
+++ plugboard/utils/__init__.py
@@ -1,8 +1,7 @@
 """Provides utility functions for use throughout the code."""
 
 from plugboard.utils.as_dict_mixin import AsDictMixin
-from plugboard.utils.class_loader import ClassLoader
 from plugboard.utils.registry import Registry
 
 
-__all__ = ["AsDictMixin", "ClassLoader", "Registry"]
+__all__ = ["AsDictMixin", "Registry"]
