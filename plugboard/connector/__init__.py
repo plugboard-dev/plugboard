40bf7b08d458c8390bc96ba3645d53d0ee782dbd
Toby Coleman <toby@tobycoleman.com>
2024-09-21 15:17:20 +0100

    Add AsyncioChannelBuilder

--- plugboard/connector/__init__.py
+++ plugboard/connector/__init__.py
@@ -1,16 +1,17 @@
 """Connector submodule providing functionality related to component connectors and data exchange."""
 
-from plugboard.connector.asyncio_channel import AsyncioChannel
+from plugboard.connector.asyncio_channel import AsyncioChannel, AsyncioChannelBuilder
 from plugboard.connector.channel import Channel, ChannelClosedError
 from plugboard.connector.channel_builder import ChannelBuilder, ChannelBuilderRegistry
 from plugboard.connector.connector import Connector
 
 
 __all__ = [
+    "AsyncioChannel",
+    "AsyncioChannelBuilder",
     "Connector",
     "Channel",
     "ChannelBuilder",
     "ChannelBuilderRegistry",
-    "AsyncioChannel",
     "ChannelClosedError",
 ]
