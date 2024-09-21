285cc3804b6d3cc8d53cd1bfa436fbc7db549abd
Toby Coleman <toby@tobycoleman.com>
2024-09-21 13:51:17 +0100

    Add generic registry

--- plugboard/utils/__init__.py
+++ plugboard/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from plugboard.utils.as_dict_mixin import AsDictMixin
 from plugboard.utils.class_loader import ClassLoader
+from plugboard.utils.registry import Registry
 
 
-__all__ = ["AsDictMixin", "ClassLoader"]
+__all__ = ["AsDictMixin", "ClassLoader", "Registry"]
---
+++ plugboard/utils/registry.py
@@ -0,0 +1,49 @@
+"""Provides a generic registry for Plugboard objects."""
+
+import typing as _t
+
+
+T = _t.TypeVar("T")
+
+
+class Registry(_t.Generic[T]):
+    """A registry of Plugboard classes."""
+
+    classes = {}
+
+    @classmethod
+    def register(cls, plugboard_class: type[T], key: _t.Optional[_t.Hashable] = None) -> None:
+        """Register a class.
+
+        Args:
+            plugboard_class: The class to register.
+            key: Optional; The key to register the class under.
+        """
+        key = key or f"{plugboard_class.__module__}.{plugboard_class.__qualname__}"
+        cls.classes[key] = plugboard_class
+
+    @classmethod
+    def get_class(cls, plugboard_class: _t.Hashable) -> type[T]:
+        """Returns a class from the registry.
+
+        Args:
+            plugboard_class: The key corresponding to the required class.
+
+        Returns:
+            The class.
+        """
+        return cls.classes[plugboard_class]
+
+    @classmethod
+    def build_object(cls, plugboard_class: _t.Hashable, *args: _t.Any, **kwargs: _t.Any) -> T:
+        """Builds a Plugboard object.
+
+        Args:
+            plugboard_class: The key corresponding to the required class.
+            *args: Positional arguments to pass to the class constructor.
+            **kwargs: Keyword arguments to pass to the class constructor.
+
+        Returns:
+            An object of the required class.
+        """
+        return cls.classes[plugboard_class](*args, **kwargs)
