906684a0ae047f8784e2c8d6a2c202c3ba012598
Toby Coleman <toby@tobycoleman.com>
2024-09-21 15:46:01 +0100

    Test component registry

---
+++ tests/integration/test_component.py
@@ -0,0 +1,20 @@
+"""Integration tests for the `Component` and `ComponentRegistry`."""
+
+import pytest
+
+from plugboard.component import ComponentRegistry
+from tests.integration.test_process_with_components_run import A, B
+
+
+@pytest.mark.anyio
+async def test_component_registry() -> None:
+    """Tests the `ComponentRegistry`."""
+    # Must load components from string name
+    a = ComponentRegistry.get_class("tests.integration.test_process_with_components_run.A")
+    isinstance(a, A)
+    b = ComponentRegistry.get_class("tests.integration.test_process_with_components_run.B")
+    isinstance(b, B)
+    with pytest.raises(KeyError):
+        ComponentRegistry.get_class(
+            "tests.integration.test_process_with_components_run.does_not_exist"
+        )
