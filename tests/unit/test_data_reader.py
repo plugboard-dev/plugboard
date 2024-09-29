aa8f0fcae6db9bb1a6e827e42f341a5d13e1daa6
Toby Coleman <toby@tobycoleman.com>
2024-09-29 15:19:44 +0100

    Extend test

--- tests/unit/test_data_reader.py
+++ tests/unit/test_data_reader.py
@@ -39,17 +39,25 @@ class MockDataReader(DataReader):
 
 @pytest.mark.anyio
 @pytest.mark.parametrize("chunk_size", [None, 2, 10])
-async def test_data_reader(chunk_size: _t.Optional[int]) -> None:
+@pytest.mark.parametrize("field_names", [["x", "z"], ["x", "y", "z"]])
+async def test_data_reader(chunk_size: _t.Optional[int], field_names: list[str]) -> None:
     """Test the DataReader class."""
     df = pd.DataFrame({"x": [1, 2, 3, 4, 5], "y": [6, 7, 8, 9, 10], "z": ["a", "b", "c", "d", "e"]})
     reader = MockDataReader(
-        name="data-reader", field_names=["x", "z"], chunk_size=chunk_size, df=df
+        name="data-reader", field_names=field_names, chunk_size=chunk_size, df=df
     )
     await reader.init()
     # Init must trigger first data fetch
     assert reader.total_fetches == 1
+    results = []
     while True:
         try:
             await reader.step()
+            results.append(reader.dict()["output"].copy())
         except IOStreamClosedError:
             break
+    df_results = pd.DataFrame(results)
+    # Returned data must be correct
+    pd.testing.assert_frame_equal(df_results, df[field_names])
+    # Total fetches must match number of chunks + 1 for the final empty chunk
+    assert reader.total_fetches == -(len(df) // -(chunk_size or len(df))) + 1
