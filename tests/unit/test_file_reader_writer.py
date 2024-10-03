f9f552445ce6fd79d2c531700ac0847a0d9e553e
Toby Coleman <toby@tobycoleman.com>
2024-10-03 20:20:48 +0100

    Fixtures for testing file IO locally and on cloud

---
+++ tests/unit/test_file_reader_writer.py
@@ -0,0 +1,79 @@
+"""Unit tests for the `FileReader` and `FileWriter` components."""
+
+import os
+from pathlib import Path
+import tempfile
+import typing as _t
+
+import fsspec
+from moto.moto_server.threaded_moto_server import ThreadedMotoServer
+import pandas as pd
+import pytest
+
+
+S3_IP_ADDRESS = "127.0.0.1"
+S3_PORT = 12345
+S3_URL = f"http://{S3_IP_ADDRESS}:{S3_PORT}"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def mock_s3_server() -> _t.Generator[None, None, None]:
+    """Provides a mock S3 server for testing.
+
+    Required for async tests until this is resolved:
+        https://github.com/aio-libs/aiobotocore/issues/755
+    """
+    server = ThreadedMotoServer(ip_address=S3_IP_ADDRESS, port=S3_PORT)
+    server.start()
+    if "AWS_SECRET_ACCESS_KEY" not in os.environ:
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "test-access-key"
+    if "AWS_ACCESS_KEY_ID" not in os.environ:
+        os.environ["AWS_ACCESS_KEY_ID"] = "test-key-id"
+    if "AWS_ENDPOINT_URL" not in os.environ:
+        os.environ["AWS_ENDPOINT_URL"] = S3_URL
+
+    yield
+    server.stop()
+
+
+@pytest.fixture
+def df() -> pd.DataFrame:
+    """DataFrame for testing read/write."""
+    return pd.DataFrame(
+        {"x": [1, 2, 3, 4, 5], "y": [6, 7, 8, 9, 10], "z": ["a", "b", "c", "d", "e"]}
+    )
+
+
+@pytest.fixture(scope="module", params=["local", "s3"])
+def temp_location(request) -> _t.Generator[str, None, None]:
+    """Temporary file location."""
+    if request.param == "local":
+        with tempfile.TemporaryDirectory() as temp_dir:
+            yield os.path.join(temp_dir, "")
+    elif request.param == "s3":
+        fs = fsspec.filesystem("s3")
+        fs.mkdir("s3://test-bucket/")
+        yield "s3://test-bucket/"
+    else:
+        raise ValueError(f"Unsupported storage type: {request.param}")
+
+
+@pytest.fixture(params=[".csv", ".csv.gz", ".parquet"])
+def path(df: pd.DataFrame, temp_location: str, request) -> _t.Generator[str, None, None]:
+    """File format for testing."""
+    path = f"{temp_location}test{request.param}"
+    if request.param == ".csv":
+        df.to_csv(path, index=False)
+    elif request.param == ".csv.gz":
+        df.to_csv(path, index=False, compression="gzip")
+    elif request.param == ".parquet":
+        df.to_parquet(path)
+    else:
+        raise ValueError(f"Unsupported file format: {request.param}")
+    yield path
+
+
+@pytest.mark.anyio
+async def test_file_reader(df: pd.DataFrame, path: Path) -> None:
+    """Test the `FileReader` component."""
+    pass
