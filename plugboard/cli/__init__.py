e093e5294774c50668dea1ac7567fcb5b82cd872
Toby Coleman <toby@tobycoleman.com>
2024-10-09 20:19:31 +0100

    Add rich formatting

--- plugboard/cli/__init__.py
+++ plugboard/cli/__init__.py
@@ -2,10 +2,15 @@
 
 import typer
 
+from plugboard import __version__
 from plugboard.cli.process import app as process_app
 
 
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(
+    rich_markup_mode="rich",
+    no_args_is_help=True,
+    help=f"[bold blue]Plugboard CLI[/bold blue]\n\nVersion {__version__}",
+)
 app.add_typer(process_app, name="process")
 
 if __name__ == "__main__":
--- plugboard/cli/process/__init__.py
+++ plugboard/cli/process/__init__.py
@@ -1,6 +1,28 @@
 """Plugboard Process CLI."""
 
+from pathlib import Path
+
 import typer
+from typing_extensions import Annotated
 
 
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(rich_markup_mode="rich", no_args_is_help=True)
+
+
+@app.command()
+def run(
+    config: Annotated[
+        Path,
+        typer.Option(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+            help="Path to the YAML configuration file.",
+        ),
+    ],
+):
+    """Run a Plugboard process."""
+    print("Running the process.")
