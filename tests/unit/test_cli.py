252c4c2b7e02e364c9de40e546fd02a96f3992ae
Toby Coleman <toby@tobycoleman.com>
2024-10-09 21:37:50 +0100

    CLI test

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,5 +1,7 @@
 """Unit tests for the CLI."""
 
+from unittest.mock import AsyncMock, patch
+
 from typer.testing import CliRunner
 
 from plugboard.cli import app
@@ -10,5 +12,16 @@ runner = CliRunner()
 
 def test_cli_process_run() -> None:
     """Tests the process run command."""
-    result = runner.invoke(app, ["process", "run", "tests/data/minimal-process.yaml"])
-    assert result.exit_code == 0
+    with patch("plugboard.cli.process.ProcessBuilder") as mock_process_builder:
+        mock_process = AsyncMock()
+        mock_process_builder.build.return_value = mock_process
+        result = runner.invoke(app, ["process", "run", "tests/data/minimal-process.yaml"])
+        # CLI must run without error
+        assert result.exit_code == 0
+        assert "Process complete" in result.stdout
+        # Process must be built
+        mock_process_builder.build.assert_called_once()
+        # Process must be initialised
+        mock_process.init.assert_called_once()
+        # Process must be run
+        mock_process.run.assert_called_once()
