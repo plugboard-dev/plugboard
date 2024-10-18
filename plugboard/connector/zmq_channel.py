ad4e4a3226184ad99c69357c43f150b8ca48d8ec
Toby Coleman <toby-coleman@users.noreply.github.com>
2024-10-18 14:51:44 +0000

    Revert to first approach and remove multiprocessing manager

--- plugboard/connector/zmq_channel.py
+++ plugboard/connector/zmq_channel.py
@@ -1,12 +1,13 @@
 """Provides ZMQChannel for use in multiprocessing environments."""
 
+import asyncio
+import multiprocessing as mp
 import typing as _t
 
 import zmq
 import zmq.asyncio
-from zmq.devices import ProcessDevice
 
-from plugboard.connector.serde_channel import SerdeChannel
+from plugboard.connector.channel import SerdeChannel
 
 
 ZMQ_ADDR = r"tcp://127.0.0.1"
@@ -18,32 +19,10 @@ class ZMQChannel(SerdeChannel):
     def __init__(self, *args: _t.Any, **kwargs: _t.Any) -> None:
         """Instantiates `ZMQChannel`."""
         super().__init__(*args, **kwargs)
-        device = ProcessDevice(zmq.QUEUE, zmq.PULL, zmq.PUSH)
-        self._port_send = device.bind_in_to_random_port(ZMQ_ADDR)
-        self._port_recv = device.bind_out_to_random_port(ZMQ_ADDR)
+        self._port = mp.Value("i", 0)
         self._context: _t.Optional[zmq.asyncio.Context] = None
         self._send_socket: _t.Optional[zmq.asyncio.Socket] = None
         self._recv_socket: _t.Optional[zmq.asyncio.Socket] = None
-        # Run ZeroMQ Queue device in a background process
-        device.start()
-
-    @property
-    def send_socket(self) -> zmq.asyncio.Socket:
-        """Returns socket for sending messages through the `ZMQChannel`."""
-        if not self._send_socket:
-            self._context = zmq.asyncio.Context()
-            self._send_socket = self._context.socket(zmq.PUSH)
-            self._send_socket.connect(f"{ZMQ_ADDR}:{self._port_send}")
-        return self._send_socket
-
-    @property
-    def recv_socket(self) -> zmq.asyncio.Socket:
-        """Returns socket for receiving messages through the `ZMQChannel`."""
-        if not self._recv_socket:
-            self._context = zmq.asyncio.Context()
-            self._recv_socket = self._context.socket(zmq.PULL)
-            self._recv_socket.connect(f"{ZMQ_ADDR}:{self._port_recv}")
-        return self._recv_socket
 
     async def send(self, msg: bytes) -> None:
         """Sends a message through the `ZMQChannel`.
@@ -51,8 +30,20 @@ class ZMQChannel(SerdeChannel):
         Args:
             msg: The message to be sent through the `ZMQChannel`.
         """
-        await self.send_socket.send(msg)
+        if not self._context:
+            self._context = zmq.asyncio.Context()
+            self._send_socket = self._context.socket(zmq.PUSH)
+            self._send_socket.bind(self.addr)
+            port = self._send_socket.bind_to_random_port(ZMQ_ADDR)
+            self._port.value = port
+        await self._socket.send(msg)
 
     async def recv(self) -> bytes:
         """Receives a message from the `ZMQChannel` and returns it."""
-        return await self.recv_socket.recv()
+        if not self._context:
+            self._context = zmq.asyncio.Context()
+            # Wait for port to be established by the sender
+            while not self._port.value:
+                await asyncio.sleep(0.1)
+            self._recv_socket.connect(f"{ZMQ_ADDR}:{self._port.value}")
+        return await self._socket.recv()
