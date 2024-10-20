ee8c0bb3af8782cb19bf8de73432783bbde30f7d
Chris Knight <chrisk314@gmail.com>
2024-10-20 16:40:04 +0100

    Export SqliteStateBackend from state module

--- plugboard/state/__init__.py
+++ plugboard/state/__init__.py
@@ -2,9 +2,11 @@
 
 from plugboard.state.dict_state_backend import DictStateBackend
 from plugboard.state.state_backend import StateBackend
+from plugboard.state.sqlite_state_backend import SqliteStateBackend
 
 
 __all__ = [
     "StateBackend",
     "DictStateBackend",
+    "SqliteStateBackend",
 ]
