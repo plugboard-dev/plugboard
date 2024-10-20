99f7f2238f40ab7a2630aee7e9e3ef855d5c4da1
Chris Knight <chrisk314@gmail.com>
2024-10-20 16:22:22 +0100

    Adds sql and methods for component upsert and get

--- plugboard/state/sqlite_state_backend.py
+++ plugboard/state/sqlite_state_backend.py
@@ -1,11 +1,17 @@
 """Provides `SqliteStateBackend` for single host persistent state handling."""
 
+import typing as _t
+
 from pathlib import Path
 from textwrap import dedent
 
 import aiosqlite
+import orjson
 
 from plugboard.state.state_backend import StateBackend
+
+if _t.TYPE_CHECKING:
+    from plugboard.component import Component
 
 
 STATE_CREATE_TABLE_SQL: str = dedent(
@@ -36,6 +42,14 @@ STATE_CREATE_TABLE_SQL: str = dedent(
     """
 )
 
+STATE_UPSERT_COMPONENT: str = dedent(
+    """INSERT OR REPLACE INTO component (data) VALUES (?);"""
+)
+
+STATE_GET_COMPONENT: str = dedent(
+    """SELECT data FROM component WHERE id = ?;"""
+)
+
 
 class SqliteStateBackend(StateBackend):
     """`SqliteStateBackend` handles single host persistent state."""
@@ -43,6 +57,14 @@ class SqliteStateBackend(StateBackend):
     def __init__(self, db_path: str = "plugboard.db") -> None:
         """Initializes `SqliteStateBackend` with `db_path`."""
         self.db_path = db_path
+
+    async def _get(self, key: str | tuple[str, ...], value: _t.Optional[_t.Any] = None) -> _t.Any:
+        """Returns a value from the state."""
+        pass
+
+    async def _set(self, key: str | tuple[str, ...], value: _t.Any) -> None:
+        """Sets a value in the state."""
+        pass
 
     async def _initialise_db(self) -> None:
         """Initializes the database."""
@@ -56,3 +78,19 @@ class SqliteStateBackend(StateBackend):
         if not Path(self.db_path).exists():
             await self._initialise_db()
         await super().init()
+
+    async def upsert_component(self, component: Component) -> None:
+        data = component.dict()
+        json = orjson.dumps(data)
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute(STATE_UPSERT_COMPONENT, (json,))
+            await db.commit()
+
+    async def get_component(self, component_id) -> dict:
+        async with aiosqlite.connect(self.db_path) as db:
+            cursor = await db.execute(STATE_GET_COMPONENT)
+            row = await cursor.fetchone()
+            data = row["data"]
+        component = orjson.loads(data)
+        return component
+        
