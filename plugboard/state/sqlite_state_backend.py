12c73a285e8c25a33ef2d0d613c43ea14ab6a148
Chris Knight <chrisk314@gmail.com>
2024-10-21 09:14:35 +0100

    Attempt creation of db connection in init method

--- plugboard/state/sqlite_state_backend.py
+++ plugboard/state/sqlite_state_backend.py
@@ -1,6 +1,6 @@
 """Provides `SqliteStateBackend` for single host persistent state handling."""
 
-from pathlib import Path
+from contextlib import AsyncExitStack
 from textwrap import dedent
 import typing as _t
 
@@ -147,7 +147,16 @@ class SqliteStateBackend(StateBackend):
 
     def __init__(self, db_path: str = "plugboard.db") -> None:
         """Initializes `SqliteStateBackend` with `db_path`."""
-        self.db_path = db_path
+        self._db_path: str = db_path
+        self._db_conn: _t.Optional[aiosqlite.Connection] = None
+        self._ctx: AsyncExitStack = AsyncExitStack()
+
+    @property
+    def _db(self) -> aiosqlite.Connection:
+        """Returns the database connection."""
+        if self._db_conn is None:
+            raise ValueError("Database connection not initialized.")
+        return self._db_conn
 
     async def _get(self, key: str | tuple[str, ...], value: _t.Optional[_t.Any] = None) -> _t.Any:
         """Returns a value from the state."""
@@ -160,15 +169,19 @@ class SqliteStateBackend(StateBackend):
     async def _initialise_db(self) -> None:
         """Initializes the database."""
         # Create database with a table storing json data
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(STATE_CREATE_TABLE_SQL)
-            await db.commit()
+        db = await self._ctx.enter_async_context(aiosqlite.connect(self._db_path))
+        self._db_conn = db
+        await db.execute(STATE_CREATE_TABLE_SQL)
+        await db.commit()
 
     async def init(self) -> None:
         """Initializes the `SqliteStateBackend`."""
-        if not Path(self.db_path).exists():
-            await self._initialise_db()
+        await self._initialise_db()
         await super().init()
+
+    async def destroy(self) -> None:
+        """Destroys the `SqliteStateBackend`."""
+        await self._ctx.aclose()
 
     async def upsert_process(self, process: Process) -> None:
         """Upserts a process into the state."""
@@ -176,7 +189,7 @@ class SqliteStateBackend(StateBackend):
         component_data = process_data.pop("components")
         connector_data = process_data.pop("connectors")
         process_json = orjson.dumps(process_data)
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             await db.execute(STATE_UPSERT_PROCESS_SQL, (process_json, self.job_id))
             await db.execute(STATE_SET_JOB_FOR_PROCESS_SQL, (self.job_id, process.id))
             for component in component_data:
@@ -191,7 +204,7 @@ class SqliteStateBackend(StateBackend):
 
     async def get_process(self, process_id: str) -> dict:
         """Returns a process from the state."""
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             cursor = await db.execute(STATE_GET_PROCESS_SQL, (process_id,))
             row = await cursor.fetchone()
             if row is None:
@@ -203,7 +216,7 @@ class SqliteStateBackend(StateBackend):
     async def _get_process_for_component(self, component_id: str) -> str:
         """Returns the process id for a component."""
         # TODO : Cache result
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             cursor = await db.execute(STATE_GET_PROCESS_FOR_COMPONENT_SQL, (component_id,))
             row = await cursor.fetchone()
             if row is None:
@@ -216,13 +229,13 @@ class SqliteStateBackend(StateBackend):
         process_id = self._get_process_for_component(component.id)
         component_data = component.dict()
         data_json = orjson.dumps(component_data)
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             await db.execute(STATE_UPSERT_COMPONENT_SQL, (data_json, process_id))
             await db.commit()
 
     async def get_component(self, component_id: str) -> dict:
         """Returns a component from the state."""
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             cursor = await db.execute(STATE_GET_COMPONENT_SQL, (component_id,))
             row = await cursor.fetchone()
             if row is None:
@@ -234,7 +247,7 @@ class SqliteStateBackend(StateBackend):
     async def _get_process_for_connector(self, connector_id: str) -> str:
         """Returns the process id for a connector."""
         # TODO : Cache result
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             cursor = await db.execute(STATE_GET_PROCESS_FOR_CONNECTOR_SQL, (connector_id,))
             row = await cursor.fetchone()
             if row is None:
@@ -247,13 +260,13 @@ class SqliteStateBackend(StateBackend):
         process_id = self._get_process_for_connector(connector.id)
         connector_data = connector.dict()
         data_json = orjson.dumps(connector_data)
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             await db.execute(STATE_UPSERT_CONNECTOR_SQL, (data_json, process_id))
             await db.commit()
 
     async def get_connector(self, connector_id: str) -> dict:
         """Returns a connector from the state."""
-        async with aiosqlite.connect(self.db_path) as db:
+        async with aiosqlite.connect(self._db_path) as db:
             cursor = await db.execute(STATE_GET_CONNECTOR_SQL, (connector_id,))
             row = await cursor.fetchone()
             if row is None:
