0db86168e87be1d8bcd35878b75c1dd92ac94839
Chris Knight <chrisk314@gmail.com>
2024-10-26 12:15:02 +0100

    Introduces `Exportable` protocol and uses it when saving args for later `export`

--- plugboard/utils/as_dict_mixin.py
+++ plugboard/utils/as_dict_mixin.py
@@ -3,8 +3,19 @@
 from functools import wraps
 import typing as _t
 
+import msgspec
+
 
 _SAVE_ARGS_INIT_KEY: str = "__save_args_init__"
+
+
+@_t.runtime_checkable
+class Exportable(_t.Protocol):
+    """`Exportable` protocol for objects that can be exported."""
+
+    def export(self) -> dict:
+        """Returns dict representation of object for later reconstruction."""
+        ...
 
 
 class AsDictMixin:
@@ -14,10 +25,35 @@ class AsDictMixin:
     def _save_args_wrapper(method: _t.Callable, key: str) -> _t.Callable:
         @wraps(method)
         def _wrapper(self: _t.Any, *args: _t.Any, **kwargs: _t.Any) -> None:
-            setattr(self, key, {**getattr(self, key, {}), **kwargs})
+            saved_kwargs = AsDictMixin._convert_exportable_objs(kwargs)
+            setattr(self, key, {**getattr(self, key, {}), **saved_kwargs})
             method(self, *args, **kwargs)
 
         return _wrapper
+
+    @staticmethod
+    def _convert_exportable_objs(kwargs: dict) -> dict:
+        """Recursively converts `Exportable` objects to their `export` representation."""
+        saved_kwargs = {}
+        for k, v in kwargs.items():
+            if isinstance(v, Exportable):
+                saved_kwargs[k] = v.export()
+            elif isinstance(v, dict):
+                saved_kwargs[k] = AsDictMixin._convert_exportable_objs(v)
+            elif isinstance(v, list):
+                # TODO : Why does mypy complain about the following line? The below example
+                #      : seems to be equivalent in terms of types and mypy doesn't complain...
+                # def _convert(kwargs: dict) -> dict:
+                #     saved_kwargs = {}
+                #     for k, v in kwargs.items():
+                #         if isinstance(v, list):
+                #             v = [x for x in v]
+                #             saved_kwargs[k] = v
+                #     return saved_kwargs
+                saved_kwargs[k] = [AsDictMixin._convert_exportable_objs(x) for x in v]
+            else:
+                saved_kwargs[k] = v
+        return saved_kwargs
 
     @staticmethod
     def _dict_inject_wrapper(method: _t.Callable) -> _t.Callable:
@@ -33,14 +69,22 @@ class AsDictMixin:
         cls.__init__ = AsDictMixin._save_args_wrapper(cls.__init__, _SAVE_ARGS_INIT_KEY)  # type: ignore # noqa: E501,W505
         cls.dict = AsDictMixin._dict_inject_wrapper(cls.dict)  # type: ignore
 
+    def export(self) -> dict:
+        """Returns dict representation of object for later reconstruction."""
+        return {
+            "type": _get_obj_cls_path(self),
+            "args": getattr(self, _SAVE_ARGS_INIT_KEY),
+        }
+
     def dict(self) -> dict:
         """Returns dict representation of object."""
         return {
-            "__export": {
-                "type": _get_obj_cls_path(self),
-                "args": getattr(self, _SAVE_ARGS_INIT_KEY),
-            }
+            "__export": self.export(),
         }
+
+    def json(self) -> bytes:
+        """Returns JSON representation of object as bytes."""
+        return msgspec.json.encode(self.dict())
 
 
 def _get_obj_cls_path(obj: _t.Any) -> str:
