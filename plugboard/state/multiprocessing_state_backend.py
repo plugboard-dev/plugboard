5f587958ad62fe1be430dc63b4190c36a60088ab
Toby Coleman <toby@tobycoleman.com>
2024-11-03 20:13:42 +0000

    Update with injected manager

--- plugboard/state/multiprocessing_state_backend.py
+++ plugboard/state/multiprocessing_state_backend.py
@@ -1,7 +1,10 @@
 """Provides `MultiprocessingStateBackend` class for local multiprocessing state."""
 
-from multiprocessing import DictProxy, Manager
+from multiprocessing import DictProxy
+from multiprocessing.managers import SyncManager
 import typing as _t
+
+import inject
 
 from plugboard.state.dict_state_backend import DictStateBackend
 
@@ -9,10 +12,15 @@ from plugboard.state.dict_state_backend import DictStateBackend
 class MultiprocessingStateBackend(DictStateBackend):
     """`MultiprocessingStateBackend` provides state persistence for single process runs."""
 
-    def __init__(self, *args: _t.Any, **kwargs: _t.Any) -> None:
-        """Instantiates `MultiprocessingStateBackend`."""
+    @inject.params(manager=SyncManager)
+    def __init__(self, manager: SyncManager, *args: _t.Any, **kwargs: _t.Any) -> None:  # noqa: D417
+        """Instantiates `MultiprocessingStateBackend`.
+
+        Args:
+            manager: A multiprocessing manager.
+        """
         super().__init__(*args, **kwargs)
-        self._manager = Manager()
+        self._manager = manager
         self._state: DictProxy[str, _t.Any] = self._manager.dict()
 
     async def _set(self, key: str | tuple[str, ...], value: _t.Any) -> None:  # noqa: A003
