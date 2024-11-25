8550b50f482442180ad5bcfbf6163c98dd89f507
Toby Coleman <toby-coleman@users.noreply.github.com>
2024-11-25 15:03:45 +0000

    Implement OpenAIStructuredChat

--- plugboard/library/openai.py
+++ plugboard/library/openai.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 from collections import deque
 import typing as _t
 
+from pydantic import BaseModel
+
 from plugboard.component import Component, IOController as IO
 from plugboard.exceptions import NotInitialisedError
 
@@ -12,6 +14,7 @@ try:
     # OpenAI is an optional dependency
     from openai import AsyncAzureOpenAI, AsyncOpenAI
     from openai.types.chat import (
+        ChatCompletion,
         ChatCompletionAssistantMessageParam,
         ChatCompletionMessageParam,
         ChatCompletionUserMessageParam,
@@ -37,9 +40,11 @@ class _OpenAIBase(Component, ABC):
     ) -> None:
         super().__init__(*args, **kwargs)
         self._model = model
-        self._system_prompt: list[ChatCompletionMessageParam] = system_prompt or []
+        self._system_prompt: list[ChatCompletionMessageParam] = system_prompt or [
+        ]
         # Store 2x the context window to allow for both input and output messages
-        self._messages: deque[ChatCompletionMessageParam] = deque(maxlen=context_window * 2)
+        self._messages: deque[ChatCompletionMessageParam] = deque(
+            maxlen=context_window * 2)
         self._open_ai_kwargs = open_ai_kwargs or {}
         self._client_type = client_type
         self._client: _t.Optional[AsyncOpenAI | AsyncAzureOpenAI] = None
@@ -50,10 +55,41 @@ class _OpenAIBase(Component, ABC):
         else:
             self._client = AsyncOpenAI(**self._open_ai_kwargs)
 
+    @property
+    def _prompt_messages(self) -> list[ChatCompletionMessageParam]:
+        """A list of messages to supply to the LLM as a prompt."""
+        return [
+            *self._system_prompt,
+            *self._messages,
+            ChatCompletionUserMessageParam(
+                role="user",
+                content=self.prompt,  # type: ignore
+            ),
+        ]
+
     @abstractmethod
+    async def _handle_llm(self) -> ChatCompletion:
+        """Implement LLM call here and set response output."""
+        pass
+
     async def step(self) -> None:  # noqa: D102
         if not self._client:
             raise NotInitialisedError()
+        completion = await self._handle_llm()
+        response = completion.choices[0].message.content
+        if response is not None:
+            self._messages.extend(
+                [
+                    ChatCompletionUserMessageParam(
+                        role="user",
+                        content=self.prompt,  # type: ignore
+                    ),
+                    ChatCompletionAssistantMessageParam(
+                        role="assistant",
+                        content=response,
+                    ),
+                ],
+            )
 
 
 class OpenAIChat(_OpenAIBase):
@@ -67,8 +103,6 @@ class OpenAIChat(_OpenAIBase):
     See the [OpenAI docs](https://platform.openai.com/docs/quickstart?language-preference=python)
     for more information on configuration and message types.
     """
-
-    io = IO(inputs=["prompt"], outputs=["response"])
 
     def __init__(
         self,
@@ -106,33 +140,76 @@ class OpenAIChat(_OpenAIBase):
             **kwargs,
         )
 
-    async def step(self) -> None:  # noqa: D102
-        await super().step()
-        if not self._client:
-            raise NotInitialisedError()
+    async def _handle_llm(self) -> ChatCompletion:
+        """Calls the chat completion API."""
         completion = await self._client.chat.completions.create(
-            messages=[
-                *self._system_prompt,
-                *self._messages,
-                ChatCompletionUserMessageParam(
-                    role="user",
-                    content=self.prompt,  # type: ignore
-                ),
-            ],
+            messages=self._prompt_messages,
             model=self._model,
         )
-        response = completion.choices[0].message.content
-        if response is not None:
-            self._messages.extend(
-                [
-                    ChatCompletionUserMessageParam(
-                        role="user",
-                        content=self.prompt,  # type: ignore
-                    ),
-                    ChatCompletionAssistantMessageParam(
-                        role="assistant",
-                        content=response,
-                    ),
-                ],
-            )
-        self.response = response  # type: ignore
+        self.response = completion.choices[0].message.content  # type: ignore
+        return completion
+
+
+class OpenAIStructuredChat(_OpenAIBase):
+    """`OpenAIStructuredChat` provides a component for interacting with OpenAI-compatible models.
+
+    Requires the optional `plugboard[openai]` installation. The API key can be set via the
+    `OPENAI_API_KEY` environment variable. For Azure OpenAI, use `AZURE_OPENAI_API_KEY` and
+    see [here](https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/switching-endpoints)
+    for additional configuration options.
+
+    This component will parse the response from the LLM into a Pydantic object. For more info see
+    the [OpenAI docs](https://platform.openai.com/docs/guides/structured-outputs). If the model
+    output cannot be passed the the response output from this component will be set to `None`.
+    """
+
+    def __init__(
+        self,
+        *args: _t.Any,
+        response_model: BaseModel,
+        model: str = "gpt-4o-mini",
+        system_prompt: _t.Optional[list[ChatCompletionMessageParam]] = None,
+        context_window: int = 0,
+        client_type: _t.Literal["openai", "azure"] = "openai",
+        open_ai_kwargs: _t.Optional[dict[str, _t.Any]] = None,
+        **kwargs: _t.Any,
+    ) -> None:
+        """Instantiates `OpenAIStructuredChat`.
+
+        Args:
+            *args: Additional positional arguments to pass to the underlying `Component`.
+            model: The name of the model to use.
+            response_model: A Pydantic model defining the expected response from the LLM. See the
+                [docs](https://platform.openai.com/docs/guides/structured-outputs#supported-schemas)
+                for supported response formats.
+            system_prompt: Optional; A list of prompts to provide to the model. This can include a
+                system prompt, along with user and assistant messages. Each message should contain
+                a `role` and `content` field. See the OpenAI docs for more information.
+            context_window: The number of messages to keep in the chat history. Higher values incur
+                more token costs on each call.
+            client_type: Whether to use "openai" or "azure" client.
+            open_ai_kwargs: Optional; Dictionary of keyword arguments to pass to the underlying
+                OpenAI client. Can include `base_url` to use a different OpenAI compatible API
+                endpoint, e.g. Gemini.
+            **kwargs: Additional keyword arguments to pass to the the underlying `Component`.
+        """
+        super().__init__(
+            *args,
+            model=model,
+            system_prompt=system_prompt,
+            context_window=context_window,
+            client_type=client_type,
+            open_ai_kwargs=open_ai_kwargs,
+            **kwargs,
+        )
+        self._response_model = response_model
+
+    async def _handle_llm(self) -> ChatCompletion:
+        """Calls the parsed chat completion API."""
+        completion = self._client.beta.chat.completions.parse(
+            messages=self._prompt_messages,
+            model=self._model,
+            response_format=self._response_model,
+        )
+        response = completion.choices[0].message
+        self.response = response.parsed if not response.refusal else None  # type: ignore
