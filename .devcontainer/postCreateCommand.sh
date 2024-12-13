73a75287b0f501f35afdc0c5953095d259b2d571
Toby Coleman <toby-coleman@users.noreply.github.com>
2024-12-13 14:54:30 +0000

    Update devcontainer for UV

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git config --global --add safe.directory $(pwd)
-poetry config virtualenvs.in-project true
-poetry install
-poetry run pre-commit install
+uv venv
+uv sync
+uv run pre-commit install
