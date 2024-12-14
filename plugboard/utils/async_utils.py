35b65c381143261db3eb841487842079418ccb73
Toby Coleman <toby@tobycoleman.com>
2024-12-14 20:36:46 +0000

    Fix types

--- plugboard/utils/async_utils.py
+++ plugboard/utils/async_utils.py
@@ -1,9 +1,10 @@
 """Provides utilities for working with asynchronous code."""
 
 import asyncio
+import typing as _t
 
 
-async def gather_except(*coros: asyncio.Future) -> list[asyncio.Future]:
+async def gather_except(*coros: asyncio.Future) -> list[_t.Any]:
     """Attempts to gather the given coroutines, raising any exceptions."""
     results = await asyncio.gather(*coros, return_exceptions=True)
     exceptions = [r for r in results if isinstance(r, Exception)]
