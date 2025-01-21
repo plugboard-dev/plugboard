ff75a79b1f666e841e45d9e663c8ed94d5bd9129
Toby Coleman <toby@tobycoleman.com>
2025-01-21 20:39:10 +0000

    Add utility to modify sys.path

--- plugboard/utils/__init__.py
+++ plugboard/utils/__init__.py
@@ -4,12 +4,14 @@ from plugboard.utils.async_utils import gather_except
 from plugboard.utils.dependencies import depends_on_optional
 from plugboard.utils.entities import EntityIdGen
 from plugboard.utils.export_mixin import Exportable, ExportMixin
+from plugboard.utils.path_utils import add_sys_path
 from plugboard.utils.random import gen_rand_str
 from plugboard.utils.ray import build_actor_wrapper
 from plugboard.utils.registry import ClassRegistry
 
 
 __all__ = [
+    "add_sys_path",
     "build_actor_wrapper",
     "depends_on_optional",
     "gather_except",
