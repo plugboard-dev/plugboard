ddaf6c61093082fb889699ff5ec13d0871e79e41
Toby Coleman <toby@tobycoleman.com>
2025-01-28 20:39:04 +0000

    Fix docstring

--- plugboard/utils/path_utils.py
+++ plugboard/utils/path_utils.py
@@ -8,7 +8,7 @@ from typing import Iterator
 
 @contextlib.contextmanager
 def add_sys_path(path: str | os.PathLike) -> Iterator:
-    """Temporarily `path` to `sys.path`."""
+    """Temporarily add `path` to `sys.path`."""
     path = os.fspath(path)
     try:
         sys.path.insert(0, path)
