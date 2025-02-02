95b848edf01af7b6a4205e469c510154349c8c85
Chris Knight <chrisk314@gmail.com>
2025-02-02 20:14:10 +0000

    Different ZMQConnector implementations based on connector mode

--- plugboard/connector/zmq_channel.py
+++ plugboard/connector/zmq_channel.py
@@ -1,11 +1,14 @@
 """Provides ZMQChannel for use in multiprocessing environments."""
 
+from __future__ import annotations
+
+from abc import ABC
 import typing as _t
 
 from plugboard.connector.connector import Connector
 from plugboard.connector.serde_channel import SerdeChannel
 from plugboard.exceptions import ChannelSetupError
-from plugboard.schemas.connector import ConnectorMode
+from plugboard.schemas.connector import ConnectorMode, ConnectorSpec
 from plugboard.utils import depends_on_optional, gen_rand_str
 
 
@@ -77,7 +80,7 @@ class ZMQChannel(SerdeChannel):
         self._is_recv_closed = True
 
 
-class ZMQConnector(Connector):
+class ZMQConnector(Connector, ABC):
     """`ZMQConnector` connects components using `ZMQChannel`."""
 
     # FIXME : If multiple workers call `connect_send` they will each see `_send_channel` null
@@ -100,10 +103,32 @@ class ZMQConnector(Connector):
         self._recv_channel: _t.Optional[ZMQChannel] = None
         self._confirm_msg = f"{ZMQ_CONFIRM_MSG}:{gen_rand_str()}".encode()
 
+    def __new__(cls, spec: ConnectorSpec, *args: _t.Any, **kwargs: _t.Any) -> ZMQConnector:  # noqa: D102
+        if cls is ZMQConnector:
+            match spec.mode:
+                case ConnectorMode.PIPELINE:
+                    return ZMQPipelineConnector(spec, *args, **kwargs)
+                case ConnectorMode.PUBSUB:
+                    return ZMQPubsubConnector(spec, *args, **kwargs)
+                case _:
+                    raise ValueError(f"Unsupported connector mode: {spec.mode}")
+        return super().__new__(cls)
+
     @property
     def zmq_address(self) -> str:
         """The ZMQ address used for communication."""
         return self._zmq_address
+
+
+class ZMQPipelineConnector(ZMQConnector):
+    """`ZMQPipelineConnector` connects components in pipeline mode using `ZMQChannel`."""
+
+    def __new__(cls, spec: ConnectorSpec, *args: _t.Any, **kwargs: _t.Any) -> ZMQPipelineConnector:  # noqa: D102
+        if spec.mode != ConnectorMode.PIPELINE:
+            raise ValueError("ZMQPipelineConnector only supports `pipeline` type connections.")
+        obj = super().__new__(cls, spec, *args, **kwargs)
+        obj = _t.cast(ZMQPipelineConnector, obj)
+        return obj
 
     async def connect_send(self) -> ZMQChannel:
         """Returns a `ZMQChannel` for sending messages."""
@@ -130,6 +155,25 @@ class ZMQConnector(Connector):
             raise ChannelSetupError("Channel confirmation message mismatch")
         self._recv_channel = ZMQChannel(recv_socket=recv_socket, maxsize=self._maxsize)
         return self._recv_channel
+
+
+class ZMQPubsubConnector(ZMQConnector):
+    """`ZMQPubsubConnector` connects components in pubsub mode using `ZMQChannel`."""
+
+    def __new__(cls, spec: ConnectorSpec, *args: _t.Any, **kwargs: _t.Any) -> ZMQPubsubConnector:  # noqa: D102
+        if spec.mode != ConnectorMode.PUBSUB:
+            raise ValueError("ZMQPubsubConnector only supports `pub-sub` type connections.")
+        obj = super().__new__(cls, spec, *args, **kwargs)
+        obj = _t.cast(ZMQPubsubConnector, obj)
+        return obj
+
+    async def connect_send(self) -> ZMQChannel:
+        """Returns a `ZMQChannel` for sending pubsub messages."""
+        raise NotImplementedError("Pubsub mode is not yet implemented")
+
+    async def connect_recv(self) -> ZMQChannel:
+        """Returns a `ZMQChannel` for receiving pubsub messages."""
+        raise NotImplementedError("Pubsub mode is not yet implemented")
 
 
 def _create_socket(
