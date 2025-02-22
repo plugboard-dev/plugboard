eca5bc6002145beff4c4ba1be5694def8d6d7156
Chris Knight <chrisk314@gmail.com>
2025-02-22 22:42:24 +0000

    Improves inheritance test

--- tests/unit/test_component_io_inheritance.py
+++ tests/unit/test_component_io_inheritance.py
@@ -8,12 +8,25 @@ import pytest
 
 from plugboard.component import IOController as IO
 from plugboard.component.component import Component
+from plugboard.events import Event
+
+
+class EventType1(Event):
+    """An event type for testing."""
+
+    type: _t.ClassVar[str] = "event_1"
+
+
+class EventType2(Event):
+    """An event type for testing."""
+
+    type: _t.ClassVar[str] = "event_2"
 
 
 @pytest.mark.parametrize("io_args, exc", [({}, ValueError), ({"inputs": ["in_1", "in_2"]}, None)])
 def test_io_inheritance(io_args: dict[str, _t.Any], exc: _t.Optional[type[Exception]]) -> None:
     """Tests that `Component` subclasses inherit `IOController` attributes."""
-    with pytest.raises(exc) if exc else nullcontext():
+    with pytest.raises(exc) if exc is not None else nullcontext():
 
         class _A(Component):
             io: IO = IO(**io_args)
@@ -23,11 +36,25 @@ def test_io_inheritance(io_args: dict[str, _t.Any], exc: _t.Optional[type[Except
         for k in {"inputs", "outputs", "input_events", "output_events"} - set(io_args.keys()):
             assert set(getattr(_A.io, k)) == set(getattr(Component.io, k))
 
+    if exc is not None:
+        return
+
+    class _B(_A):
+        io: IO = IO(
+            inputs=["in_3"],
+            outputs=["out_1"],
+            input_events=[EventType1],
+            output_events=[EventType2],
+        )
+
+    for k in {"inputs", "outputs", "input_events", "output_events"}:
+        assert set(getattr(_B.io, k)) > set(getattr(_A.io, k))
+
 
 @pytest.mark.parametrize("io_args, exc", [({}, None), ({"inputs": ["in_1", "in_2"]}, None)])
 def test_io_inheritance_abc(io_args: dict[str, _t.Any], exc: _t.Optional[type[Exception]]) -> None:
     """Tests that abstract `Component` subclasses inherit `IOController` attributes."""
-    with pytest.raises(exc) if exc else nullcontext():
+    with pytest.raises(exc) if exc is not None else nullcontext():
 
         class _A(Component, ABC):
             io: IO = IO(**io_args)
@@ -36,3 +63,17 @@ def test_io_inheritance_abc(io_args: dict[str, _t.Any], exc: _t.Optional[type[Ex
             assert set(getattr(_A.io, k)) > set(getattr(Component.io, k))
         for k in {"inputs", "outputs", "input_events", "output_events"} - set(io_args.keys()):
             assert set(getattr(_A.io, k)) == set(getattr(Component.io, k))
+
+    if exc is not None:
+        return
+
+    class _B(_A):
+        io: IO = IO(
+            inputs=["in_3"],
+            outputs=["out_1"],
+            input_events=[EventType1],
+            output_events=[EventType2],
+        )
+
+    for k in {"inputs", "outputs", "input_events", "output_events"}:
+        assert set(getattr(_B.io, k)) > set(getattr(_A.io, k))
