20c30996466fa4c1e562636460fbcdffd965a687
Toby Coleman <toby@tobycoleman.com>
2025-03-01 19:49:56 +0000

    Move to integration test

--- tests/integration/test_process_validation.py
+++ tests/integration/test_process_validation.py
@@ -1,10 +1,13 @@
 """Tests validation on `Process` objects."""
 
 import re
+import typing as _t
 
 import pytest
 from structlog.testing import capture_logs
 
+from plugboard import exceptions
+from plugboard.component import Component, IOController as IO
 from plugboard.process import LocalProcess
 from tests.integration.test_process_with_components_run import A, C
 
@@ -31,3 +34,24 @@ async def test_missing_connections() -> None:
     logs = filter_logs(logs, "level", "error")
     logs = filter_logs(logs, "event", "input not connected")
     assert logs, "Logs do not indicate missing connection"
+
+
+@pytest.mark.anyio
+async def test_component_validation() -> None:
+    """Tests that invalid components are detected."""
+
+    class NoSuperCall(Component):
+        io = IO(inputs=["x"], outputs=["y"])
+
+        def __init__(*args: _t.Any, **kwargs: _t.Any):
+            pass
+
+        async def step(self) -> None:
+            self.y = self.x
+
+    process = LocalProcess(
+        components=[A(name="a", iters=10), NoSuperCall(name="test-no-super")], connectors=[]
+    )
+
+    with pytest.raises(exceptions.ComponentValidationError):
+        await process.init()
