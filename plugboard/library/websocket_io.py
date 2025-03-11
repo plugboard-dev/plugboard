8da054a0bc09a5acf27b41a1d131fad56d6fae88
Toby Coleman <toby@tobycoleman.com>
2025-03-11 19:24:38 +0000

    Refactor websocket components to use common base class

--- plugboard/library/websocket_io.py
+++ plugboard/library/websocket_io.py
@@ -1,5 +1,6 @@
 """Provides `WebsocketReader` and `WebsocketWriter` realtime data in Plugboard."""
 
+from abc import ABC
 from contextlib import AsyncExitStack
 import typing as _t
 
@@ -18,46 +19,39 @@ except ImportError:
     pass
 
 
-class WebsocketReader(Component):
-    """Reads data from a websocket connection."""
+class WebsocketArgsDict(ComponentArgsDict):
+    """Specification of the `WebsocketBase` constructor arguments."""
 
-    io = IOController(outputs=["message"])
+    uri: str
+    connect_args: _t.NotRequired[dict[str, _t.Any] | None]
+
+
+class WebsocketBase(Component, ABC):
+    """Base [`Component`][plugboard.component.Component] for websocket connections.
+
+    See https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html for possible
+        connection arguments.
+    """
+
+    io = IOController()
 
     @depends_on_optional("websockets")
     def __init__(
         self,
         uri: str,
         connect_args: dict[str, _t.Any] | None = None,
-        initial_message: _t.Any | None = None,
-        skip_messages: int = 0,
-        parse_json: bool = False,
         **kwargs: _t.Unpack[ComponentArgsDict],
     ) -> None:
-        """Instantiates the `WebsocketReader`.
-
-        See https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html for possible
-        connection arguments that can be passed using `connect_args`. This `WebsocketReader` will
-        run until interrupted, and automatically reconnect if the server connection is lost.
+        """Instantiates the [`Component`][plugboard.component.Component].
 
         Args:
             uri: The URI of the WebSocket server.
             connect_args: Optional; Additional arguments to pass to the WebSocket connection.
-            initial_message: Optional; The initial message to send to the WebSocket server on
-                connection. Can be used to subscribe to a specific topic.
-            skip_messages: The number of messages to ignore before starting to read messages.
-            parse_json: Whether to parse the received data as JSON.
             **kwargs: Additional keyword arguments for [`Component`][plugboard.component.Component].
         """
         super().__init__(**kwargs)
         self._uri = uri
         self._connect_args = {"process_exception": self._process_exception, **(connect_args or {})}
-        if initial_message is not None:
-            self._initial_message = json.encode(initial_message) if parse_json else initial_message
-        else:
-            self._initial_message = None
-        self._skip_messages = skip_messages
-        self._skip_count = 0
-        self._parse_json = parse_json
         self._connection_success = False
         self._ctx = AsyncExitStack()
         self._conn: Connection | None = None
@@ -79,6 +73,50 @@ class WebsocketReader(Component):
 
     async def _get_conn(self) -> "Connection":
         conn = await self._ctx.enter_async_context(await anext(self._conn_iter))
+        return conn
+
+    async def destroy(self) -> None:
+        """Closes the websocket connection."""
+        await self._ctx.aclose()
+
+
+class WebsocketReader(WebsocketBase):
+    """Reads data from a websocket connection."""
+
+    io = IOController(outputs=["message"])
+
+    def __init__(
+        self,
+        initial_message: _t.Any | None = None,
+        skip_messages: int = 0,
+        parse_json: bool = False,
+        **kwargs: _t.Unpack[WebsocketArgsDict],
+    ) -> None:
+        """Instantiates the `WebsocketReader`.
+
+        See https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html for possible
+        connection arguments that can be passed using `connect_args`. This `WebsocketReader` will
+        run until interrupted, and automatically reconnect if the server connection is lost.
+
+        Args:
+            initial_message: Optional; The initial message to send to the WebSocket server on
+                connection. Can be used to subscribe to a specific topic.
+            skip_messages: The number of messages to ignore before starting to read messages.
+            parse_json: Whether to parse the received data as JSON.
+            **kwargs: Additional keyword arguments for
+                [`WebsocketBase`][plugboard.library.websocket_io.WebsocketBase].
+        """
+        super().__init__(**kwargs)
+        if initial_message is not None:
+            self._initial_message = json.encode(initial_message) if parse_json else initial_message
+        else:
+            self._initial_message = None
+        self._skip_messages = skip_messages
+        self._skip_count = 0
+        self._parse_json = parse_json
+
+    async def _get_conn(self) -> "Connection":
+        conn = await super()._get_conn()
         if self._initial_message is not None:
             self._logger.info(f"Sending initial message", message=self._initial_message)
             await conn.send(self._initial_message)
@@ -100,12 +138,8 @@ class WebsocketReader(Component):
             self._logger.warning(f"Connection to {self._uri} closed, will reconnect...")
             self._conn = None
 
-    async def destroy(self) -> None:
-        """Closes the websocket connection."""
-        await self._ctx.aclose()
 
-
-class WebsocketWriter(Component):
+class WebsocketWriter(WebsocketBase):
     """Writes data to a websocket connection."""
 
     io = IOController(inputs=["message"])
@@ -113,10 +147,8 @@ class WebsocketWriter(Component):
     @depends_on_optional("websockets")
     def __init__(
         self,
-        uri: str,
-        connect_args: dict[str, _t.Any] | None = None,
         parse_json: bool = False,
-        **kwargs: _t.Unpack[ComponentArgsDict],
+        **kwargs: _t.Unpack[WebsocketArgsDict],
     ) -> None:
         """Instantiates the `WebsocketWriter`.
 
@@ -124,25 +156,12 @@ class WebsocketWriter(Component):
         connection arguments that can be passed using `connect_args`.
 
         Args:
-            uri: The URI of the WebSocket server.
-            connect_args: Optional; Additional arguments to pass to the websocket connection.
             parse_json: Whether to convert the data to JSON before sending.
-            **kwargs: Additional keyword arguments for [`Component`][plugboard.component.Component].
+            **kwargs: Additional keyword arguments for
+                [`WebsocketBase`][plugboard.library.websocket_io.WebsocketBase].
         """
         super().__init__(**kwargs)
-        self._uri = uri
-        self._connect_args = {"process_exception": self._process_exception, **(connect_args or {})}
         self._parse_json = parse_json
-        self._connection_success = False
-        self._ctx = AsyncExitStack()
-
-    def _process_exception(self, exc: Exception) -> Exception | None:
-        if not self._connection_success:
-            self._logger.error("Error connecting to websocket", exc_info=exc)
-            return exc
-        # If connection was established, use the default exception handler
-        self._logger.info("Error raised in websocket connection", exc_info=exc)
-        return process_exception(exc)
 
     async def init(self) -> None:
         """Initializes the websocket connection."""
@@ -150,9 +169,6 @@ class WebsocketWriter(Component):
         self._conn = await self._get_conn()
         self._connection_success = True
         self._logger.info(f"Connected to {self._uri}")
-
-    async def _get_conn(self) -> "Connection":
-        return await self._ctx.enter_async_context(await anext(self._conn_iter))
 
     async def step(self) -> None:
         """Writes a message to the websocket connection."""
