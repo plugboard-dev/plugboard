7e6aaed4ab6ad6f4c690ad809740b8f432ba72c7
Chris Knight <chrisk314@gmail.com>
2025-03-15 19:22:48 +0000

    Refactors ZMQPipelineConnectorV2

--- plugboard/connector/zmq_channel.py
+++ plugboard/connector/zmq_channel.py
@@ -190,71 +190,6 @@ class _ZMQPipelineConnector(_ZMQConnector):
         return self._recv_channel
 
 
-class _ZMQPipelineConnectorV2(_ZMQConnector):
-    """`_ZMQPipelineConnectorV2` connects components in pipeline mode using `ZMQChannel`."""
-
-    def __init__(self, *args: _t.Any, **kwargs: _t.Any) -> None:
-        super().__init__(*args, **kwargs)
-        self._topic = str(self.spec.id)
-        self._xsub_port: _t.Optional[int] = None
-        self._xpub_port: _t.Optional[int] = None
-
-        self._push_socket: zmq.asyncio.Socket = create_socket(
-            zmq.PUSH, [(zmq.SNDHWM, self._maxsize)]
-        )
-        self._push_port: int = self._push_socket.bind_to_random_port("tcp://*")
-
-        self._proxy_task: asyncio.Task = asyncio.create_task(self._start_proxy())
-        _zmq_proxy_tasks.add(self._proxy_task)
-
-    def __getstate__(self) -> dict:
-        state = self.__dict__.copy()
-        for attr in ("_push_socket", "_proxy_task"):
-            if attr in state:
-                del state[attr]
-        return state
-
-    async def _start_proxy(self) -> None:
-        _, xpub_port = await self._get_proxy_ports()
-
-        sub_socket = create_socket(
-            zmq.SUB, [(zmq.RCVHWM, self._maxsize), (zmq.SUBSCRIBE, self._topic.encode("utf8"))]
-        )
-        sub_socket.connect(f"{self._zmq_address}:{xpub_port}")
-        try:
-            while True:
-                msg = await sub_socket.recv_multipart()
-                await self._push_socket.send_multipart(msg)
-        finally:
-            sub_socket.close(linger=0)
-            self._push_socket.close(linger=0)
-
-    @inject
-    async def _get_proxy_ports(
-        self, zmq_proxy: ZMQProxy = Provide[DI.zmq_proxy]
-    ) -> tuple[int, int]:
-        if self._xsub_port is not None and self._xpub_port is not None:
-            return self._xsub_port, self._xpub_port
-        await zmq_proxy.start_proxy(zmq_address=self._zmq_address, maxsize=self._maxsize)
-        self._xsub_port, self._xpub_port = await zmq_proxy.get_proxy_ports()
-        return self._xsub_port, self._xpub_port
-
-    async def connect_send(self) -> ZMQChannel:
-        """Returns a `ZMQChannel` for sending messages."""
-        await self._get_proxy_ports()
-        send_socket = create_socket(zmq.PUB, [(zmq.SNDHWM, self._maxsize)])
-        send_socket.connect(f"{self._zmq_address}:{self._xsub_port}")
-        await asyncio.sleep(0.1)  # Ensure connections established before first send. Better way?
-        return ZMQChannel(send_socket=send_socket, topic=self._topic, maxsize=self._maxsize)
-
-    async def connect_recv(self) -> ZMQChannel:
-        """Returns a `ZMQChannel` for receiving messages."""
-        recv_socket = create_socket(zmq.PULL, [(zmq.RCVHWM, self._maxsize)])
-        recv_socket.connect(f"{self._zmq_address}:{self._push_port}")
-        await asyncio.sleep(0.1)  # Ensure connections established before first send. Better way?
-        return ZMQChannel(recv_socket=recv_socket, topic=self._topic, maxsize=self._maxsize)
-
-
 class _ZMQPubsubConnector(_ZMQConnector):
     """`_ZMQPubsubConnector` connects components in pubsub mode using `ZMQChannel`."""
 
@@ -347,6 +282,51 @@ class _ZMQPubsubConnectorProxy(_ZMQConnector):
         ]
         recv_socket = create_socket(zmq.SUB, socket_opts)
         recv_socket.connect(f"{self._zmq_address}:{self._xpub_port}")
+        await asyncio.sleep(0.1)  # Ensure connections established before first send. Better way?
+        return ZMQChannel(recv_socket=recv_socket, topic=self._topic, maxsize=self._maxsize)
+
+
+class _ZMQPipelineConnectorV2(_ZMQPubsubConnectorProxy):
+    """`_ZMQPipelineConnectorV2` connects components in pipeline mode using `ZMQChannel`."""
+
+    def __init__(self, *args: _t.Any, **kwargs: _t.Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._topic = str(self.spec.id)
+
+        self._push_socket: zmq.asyncio.Socket = create_socket(
+            zmq.PUSH, [(zmq.SNDHWM, self._maxsize)]
+        )
+        self._push_port: int = self._push_socket.bind_to_random_port("tcp://*")
+
+        self._push_proxy_task: asyncio.Task = asyncio.create_task(self._start_push_proxy())
+        _zmq_proxy_tasks.add(self._push_proxy_task)
+
+    def __getstate__(self) -> dict:
+        state = self.__dict__.copy()
+        for attr in ("_push_socket", "_push_proxy_task"):
+            if attr in state:
+                del state[attr]
+        return state
+
+    async def _start_push_proxy(self) -> None:
+        _, xpub_port = await self._get_proxy_ports()
+
+        sub_socket = create_socket(
+            zmq.SUB, [(zmq.RCVHWM, self._maxsize), (zmq.SUBSCRIBE, self._topic.encode("utf8"))]
+        )
+        sub_socket.connect(f"{self._zmq_address}:{xpub_port}")
+        try:
+            while True:
+                msg = await sub_socket.recv_multipart()
+                await self._push_socket.send_multipart(msg)
+        finally:
+            sub_socket.close(linger=0)
+            self._push_socket.close(linger=0)
+
+    async def connect_recv(self) -> ZMQChannel:
+        """Returns a `ZMQChannel` for receiving messages."""
+        recv_socket = create_socket(zmq.PULL, [(zmq.RCVHWM, self._maxsize)])
+        recv_socket.connect(f"{self._zmq_address}:{self._push_port}")
         await asyncio.sleep(0.1)  # Ensure connections established before first send. Better way?
         return ZMQChannel(recv_socket=recv_socket, topic=self._topic, maxsize=self._maxsize)
 
