ecd58e24e208662e46d7630159f0a75ef2e7ca57
Chris Knight <chrisk314@gmail.com>
2025-03-31 09:30:03 +0100

    Passes logger to method for mp start method

--- plugboard/utils/di.py
+++ plugboard/utils/di.py
@@ -1,7 +1,6 @@
 """Provides a dependency injection container and utils."""
 
 import multiprocessing
-import sys
 import typing as _t
 
 import structlog
@@ -13,12 +12,15 @@ from plugboard.utils.logging import configure_logging
 from plugboard.utils.settings import Settings
 
 
-def _mp_set_start_method(use_fork: bool = False) -> _t.Iterator[None]:
+def _mp_set_start_method(
+    logger: Singleton[structlog.BoundLogger], use_fork: bool = False
+) -> _t.Iterator[None]:
     try:
-        multiprocessing.get_context(method="fork" if use_fork else "spawn")
+        method = "fork" if use_fork else "spawn"
+        multiprocessing.get_context(method=method)
+        logger.debug(f"Set multiprocessing start method to {method}")
     except ValueError:
-        print("Failed to set multiprocessing start method", file=sys.stderr, flush=True)
-        pass
+        logger.warning("Failed to set multiprocessing start method")
     yield
 
 
@@ -32,7 +34,7 @@ def _zmq_proxy(
         try:
             zmq_proxy.terminate(timeout=5.0)
         except RuntimeError as e:
-            logger.warning(f"Error during ZMQProxy termination: {e}", file=sys.stderr)
+            logger.warning(f"Error during ZMQProxy termination: {e}")
 
 
 def _logger(settings: Settings) -> structlog.BoundLogger:
@@ -45,5 +47,7 @@ class DI(BaseContainer):
 
     settings: Singleton[Settings] = Singleton(Settings)
     logger: Singleton[structlog.BoundLogger] = Singleton(_logger, settings.cast)
-    mp_ctx: Resource[None] = Resource(_mp_set_start_method, settings.flags.multiprocessing_fork)
+    mp_ctx: Resource[None] = Resource(
+        _mp_set_start_method, logger, use_fork=settings.flags.multiprocessing_fork
+    )
     zmq_proxy: Resource[ZMQProxy] = Resource(_zmq_proxy, mp_ctx, logger)
