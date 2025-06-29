e3626d6757ad647b40f62659f98a35e1b684d87d
Chris Knight <chrisk314@gmail.com>
2025-06-29 14:37:10 +0200

    Adds comment for method

--- plugboard/component/component.py
+++ plugboard/component/component.py
@@ -264,6 +264,12 @@ class Component(ABC, ExportMixin):
         return _wrapper
 
     async def _io_read_with_status_check(self) -> None:
+        """Repeatedly attempts to read from IO controller with periodic status checks.
+
+        Each read attempt is made with a timeout. If the read times out, the status of the
+        process is checked. If the process is in a failed state, the component status is set to
+        `STOPPED` and a `ProcessStatusError` is raised; otherwise another read attempt is made.
+        """
         while True:
             try:
                 await asyncio.wait_for(self.io.read(), timeout=IO_READ_TIMEOUT_SECONDS)
