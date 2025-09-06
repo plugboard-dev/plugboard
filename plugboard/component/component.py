6d38d95ee1d71ccef18b9b779dd8f66b10c94bc2
Chris Knight <chrisk314@gmail.com>
2025-09-06 19:55:20 +0200

    fixup! Passes timeout for iocontroller from component

--- plugboard/component/component.py
+++ plugboard/component/component.py
@@ -321,7 +321,8 @@ class Component(ABC, ExportMixin):
 
     @cached_property
     def _has_event_inputs(self) -> bool:
-        return len(self.io.input_events) > 0
+        input_events = set([evt.safe_type() for evt in self.io.input_events])
+        return len(input_events - {StopEvent.safe_type()}) > 0
 
     @cached_property
     def _has_inputs(self) -> bool:
