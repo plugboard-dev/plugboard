e75ba52411f96679e5732c635347c88b56716cb6
Toby Coleman <toby@tobycoleman.com>
2025-11-15 14:12:02 +0000

    Update docs to cover parameters

---
+++ examples/tutorials/002_complex_processes/parameters.py
@@ -0,0 +1,42 @@
+"""An example using process parameters."""
+
+# fmt: off
+import asyncio
+
+from plugboard.connector import AsyncioConnector
+from plugboard.process import LocalProcess
+from plugboard.schemas import ConnectorSpec
+
+from components import Random, Save, ScaleFromParameter, Sum
+
+
+async def main() -> None:
+    # --8<-- [start:main]
+    connect = lambda in_, out_: AsyncioConnector(
+        spec=ConnectorSpec(source=in_, target=out_)
+    )
+    process = LocalProcess(
+        components=[
+            Random(name="random", iters=5, low=0, high=10),
+            ScaleFromParameter(name="scale_a"),
+            ScaleFromParameter(name="scale_b"),  # (2)!
+            Sum(name="sum"),
+            Save(name="save-input", path="input.txt"),
+            Save(name="save-output", path="output.txt"),
+        ],
+        connectors=[
+            connect("random.x", "save-input.value_to_save"),
+            connect("random.x", "scale_a.a"),
+            connect("random.x", "scale_b.a"),
+            connect("scale_a.x", "sum.a"),
+            connect("scale_b.x", "sum.b"),
+            connect("sum.x", "save-output.value_to_save"),
+        ],
+        parameters={"scale": 0.5},  # (1)!
+    )
+    async with process:
+        await process.run()
+    # --8<-- [end:main]
+
+if __name__ == "__main__":
+    asyncio.run(main())
