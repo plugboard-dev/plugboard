155c571cb504a42ae022a737c15847550f598a8b
Toby Coleman <toby@tobycoleman.com>
2026-02-08 14:07:49 +0000

    Typo

--- plugboard/tune/tune.py
+++ plugboard/tune/tune.py
@@ -213,7 +213,7 @@ class Tuner:
             spec: The [`ProcessSpec`][plugboard.schemas.ProcessSpec] to optimise.
 
         Returns:
-            Either one or a list of `Result` objects containing the best trial esult. Use the
+            Either one or a list of `Result` objects containing the best trial result. Use the
                 `result_grid` property to get full trial results.
         """
         self._logger.info("Running optimisation job on Ray")
